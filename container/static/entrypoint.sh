36d69969987127a23ae14364759024e6820e15e5
Ryan Ross <ryanaross@gmail.com>
2020-07-19 15:10:27 -0700

    reload instructions

--- container/static/entrypoint.sh
+++ container/static/entrypoint.sh
@@ -4,6 +4,7 @@
 
 /livegrep/bin/codesearch -index_only -dump_index /data/livegrep.idx /data/livegrep.json
 
+# To actually support reloading, need to run /livegrep/bin/livegrep-reload "localhost:9999" on cronjob
 /livegrep/bin/codesearch -load_index /data/livegrep.idx -reload_rpc -grpc "0.0.0.0:9999" &
 
 /livegrep/bin/livegrep -connect "localhost:9999" -docroot /livegrep/web -listen "0.0.0.0:8910"
